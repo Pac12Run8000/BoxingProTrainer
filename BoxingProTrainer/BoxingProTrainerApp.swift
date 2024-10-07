326d98c43cf83100ff860fb8eb86f341eb592fc5
Norbert Grover <norbert298@gmail.com>
2024-10-06 19:26:11 -0700

    Initial Commit

---
+++ BoxingProTrainer/BoxingProTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxingProTrainerApp.swift
+//  BoxingProTrainer
+//
+//  Created by Norbert Grover on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct BoxingProTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
